a2d286882deb9001369c8935bf90812a76985353
teaearlgraycold <supacoofoo@gmail.com>
2017-08-29 03:01:34 -0400

    Add documentation to read_commits.rb

--- read_commits.rb
+++ read_commits.rb
@@ -1,3 +1,11 @@
+# read_commits.rb
+#
+# Usage: read_commits.rb <username> <github api token>
+# Read a user's commits and report the time of the commit, the number of lines
+# changed, and the language of each file in the commit. Data is sent to stdout
+# so that it may be stored as a CSV file or passed into a program capable of
+# processing CSV data.
+
 require 'octokit'
 require 'linguist'
 
@@ -7,6 +15,7 @@ class VisBlob < Linguist::Blob
   end
 end
 
+# Determine the language of a given file
 def get_lang(client, repo, file)
   content = client.get file[:raw_url]
   blob = VisBlob.new(file[:filename], content)
@@ -22,6 +31,7 @@ def get_lang(client, repo, file)
   end
 end
 
+# Calculate stats for an event on the user's timeline
 def event_stats(client, event)
   repo = client.repository(event[:repo][:name])
   event[:payload][:commits].map do |commit|
@@ -29,6 +39,7 @@ def event_stats(client, event)
   end
 end
 
+# Calculate stats for a specific commit within an event
 def commit_stats(client, repo, commit, timestamp)
   client.commit(repo[:full_name], commit[:sha])[:files].map do |file|
     lang = get_lang(client, repo, file)
@@ -39,6 +50,7 @@ def commit_stats(client, repo, commit, timestamp)
   end
 end
 
+# Run through all commits available
 def analyze_commits(user, token)
   client = Octokit::Client.new(access_token: token)
 
